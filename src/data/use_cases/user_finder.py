f1d089fa65fd5a5c2c597230ee62cfa7e442e9c0
Marcos Vinicius <marcosvinicius@macBook.local>
2023-12-06 21:58:14 -0300

    feat: add test to user finder

--- src/data/use_cases/user_finder.py
+++ src/data/use_cases/user_finder.py
@@ -1,6 +1,7 @@
-from typing import Dict
+from typing import Dict, List
 
 from src.data.interfaces.users_repository import UserRepositoryInterface
+from src.domain.models.users import Users
 from src.domain.use_cases.user_finder import UserFinder as UserFinderInterface
 
 
@@ -12,7 +13,30 @@ class UserFinder(UserFinderInterface):
         return self.__user_repository.insert_user(first_name, last_name, age)
 
     def find(self, first_name) -> Dict:
-        return self.__user_repository.select_user(first_name)
+        self.__validate_name(first_name)
+
+        users = self.__search_user(first_name)
+        response = self.__format_response(users)
+        return response
 
     def delete(self, user_id: int) -> None:
         return self.__user_repository.delete_user(user_id)
+
+    @classmethod
+    def __validate_name(cls, first_name: str) -> None:
+        if not isinstance(first_name, str):
+            raise TypeError("First name must be a string")
+
+        if len(first_name) > 18:
+            raise ValueError("First name must be less than 18 characters")
+
+    def __search_user(self, first_name: str) -> List[Users]:
+        users = self.__user_repository.select_user(first_name)
+        if users == []:
+            raise ValueError("User not found")
+        return users
+
+    @classmethod
+    def __format_response(cls, users: List[Users]) -> Dict:
+        response = {"type": "Users", "count": len(users), "attributes": users}
+        return response
--- src/tests/test_user_finder.py
+++ src/tests/test_user_finder.py
@@ -11,7 +11,7 @@ from .connection_test import SessionLocalTestIntegration
 @pytest.fixture(scope="session")
 def data():
     faker = Faker()
-    mocked_first_name = faker.name()
+    mocked_first_name = faker.pystr_format(string_format=f"{{name}}".ljust(17))
     mocked_last_name = faker.name()
     mocked_age = faker.random_number(digits=2)
 
@@ -37,10 +37,37 @@ def test_create_user(data):
 def test_find_user(data):
     user_repository = UserRepository(SessionLocalTestIntegration)
     user_finder = UserFinder(user_repository)
-    user = user_finder.find(data["name"])
-    assert user[0].first_name == data["name"]
-    assert user[0].last_name == data["last_name"]
-    assert user[0].age == data["age"]
+    response = user_finder.find(data["name"])
+    assert response["attributes"][0].first_name == data["name"]
+    assert response["attributes"][0].last_name == data["last_name"]
+    assert response["attributes"][0].age == data["age"]
+
+
+def test_find_user_with_invalid_first_name():
+    user_repository = UserRepository(SessionLocalTestIntegration)
+    user_finder = UserFinder(user_repository)
+    with pytest.raises(TypeError) as exc_info:
+        user_finder.find(first_name=9_090_983)
+
+    assert str(exc_info.value) == "First name must be a string"
+
+
+def test_find_user_with_invalid_first_name_length():
+    user_repository = UserRepository(SessionLocalTestIntegration)
+    user_finder = UserFinder(user_repository)
+    with pytest.raises(ValueError) as exc_info:
+        user_finder.find(first_name="a" * 19)
+
+    assert str(exc_info.value) == "First name must be less than 18 characters"
+
+
+def test_find_user_with_user_not_found():
+    user_repository = UserRepository(SessionLocalTestIntegration)
+    user_finder = UserFinder(user_repository)
+    with pytest.raises(ValueError) as exc_info:
+        user_finder.find(first_name="JONH DOE")
+
+    assert str(exc_info.value) == "User not found"
 
 
 def test_delete_user():
